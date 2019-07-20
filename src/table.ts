1b08b742397ce0c2f96cc85d8c842a37ec77e2f8
inker <anton.veselyev@gmail.com>
2019-07-20 21:31:07 +0100

    update alphabet

--- src/table.ts
+++ src/table.ts
@@ -13,9 +13,9 @@ const alias = {
 
 const table: Table = {}
 // Invert 'alphabet'
-let i = 0
-for (const letter of alphabet) {
-  table[letter] = i++
+for (let i = 0; i < alphabet.length; ++i) {
+  const letter = alphabet[i]
+  table[letter] = i
 }
 // Splice in 'alias'
 for (const [key, val] of Object.entries(alias)) {
