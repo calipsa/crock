60050ef39c6f1436e663b452789f2aed19de6cb9
inker <anton.veselyev@gmail.com>
2019-07-27 20:36:04 +0100

    add binToHex test

--- test/index.test.ts
+++ test/index.test.ts
@@ -3,6 +3,7 @@ import * as Uuid from 'uuid/v4'
 import alphabet from '../src/alphabet'
 import table from '../src/table'
 
+import binToHex from '../src/utils/binToHex'
 import hexToOctet from '../src/utils/hexToOctet'
 import octetToHex from '../src/utils/octetToHex'
 
@@ -40,6 +41,23 @@ describe('Testing crock', () => {
 
     expect(() => Crock.decode10crock('bad_encodi')).toThrowErrorMatchingSnapshot()
     expect(() => Crock.decode6crock('bad_en')).toThrowErrorMatchingSnapshot()
+  })
+
+  it('Should correctly convert bin to hex', () => {
+    const numbers = [
+      0,
+      1,
+      0xffffffffff,
+    ]
+    for (let i = 0; i < 200; ++i) {
+      const num = Number.parseInt(Math.random().toString(16).slice(2, 10), 16)
+      numbers.push(num)
+    }
+    for (const n of numbers) {
+      const hex = binToHex(n, 8)
+      const n2 = Number.parseInt(hex, 16)
+      expect(n2).toBe(n)
+    }
   })
 
   it('Should convert hex to octet & back', () => {
